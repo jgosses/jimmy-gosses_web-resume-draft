dc8fcc01ab2386e0c70c0d43ba9ac790eef1d78a
Jimmy Gosses <gosses.jim@gmail.com>
2018-11-29 19:11:40 -0500

    toggle mobile nav

--- js/main.js
+++ js/main.js
@@ -3,24 +3,26 @@ var navIcon = document.getElementById('hamburger-nav-icon');
 var navList = document.querySelectorAll('li.toggle-main-nav2');
 var navList2 = document.querySelectorAll('toggle-nav-icon');
 var navClose = document.querySelector('hamburger-nav-icon')
-var navList3 = document.querySelector('js-nav-build');
+var navList3 = document.querySelectorAll('js-nav-build');
 
 // if click on nav icon, toggle mobile open nav class and build out nav directions
 
 navIcon.addEventListener("click", openMobileNav => {
     if (navList.length > 0) {               // if the nav contains items    
         for (var i=0; i < navList.length; i++) {
-            navList[i].className = 'js-nav-build';      //change this from toggle-nav-icon to toggle-main-nav
+            navList[i].classList.toggle('js-nav-build');      //change this from toggle-nav-icon to toggle-main-nav
         };
-        navIcon.innerHTML = "X";
-    }
+        //navIcon.innerHTML = "X";
+    };
 })
 
 // close the mobile-view nav
 navIcon.addEventListener("click", closeMobileNav => {
-    if (navIcon.innerHTML === "X") {
-        navList3.className = 'toggle-main-nav2';
-    };
+    if (navList3.active = true) {
+        navIcon.textContent = "X";
+    }; //else {
+
+    //}
 })
 
 
